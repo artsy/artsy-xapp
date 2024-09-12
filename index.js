dd36cb24e50026918a05a7a6c6ec02b16f849eec
Matt Jones <matt.jones@artsymail.com>
2024-09-12 16:44:46 -0400

    refactor: use node native fetch

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var Emitter = require('events').EventEmitter,
-  request = require('superagent');
-
+var Emitter = require("events").EventEmitter;
 var MAX_NODE_TIMEOUT = 2147483647;
 var MIN_NODE_TIMEOUT = 1;
-var timeoutHandles = []
+var timeoutHandles = [];
 
 module.exports = new Emitter();
 
 module.exports.init = function (options, callback) {
-
   // Do a solid and just callback for test env
-  if (process.env.NODE_ENV == 'test') return callback();
+  if (process.env.NODE_ENV == "test") return callback();
 
   // Make sure there aren't any lingering timeouts.
   clearTimeouts();
 
   // Setup defaults
-  if (typeof options == 'function') {
+  if (typeof options == "function") {
     callback = options;
     var options = {};
   }
@@ -26,7 +23,7 @@ module.exports.init = function (options, callback) {
 
   // Fetch the xapp token, cache, and refresh
   fetchAndCacheToken(options, callback);
-}
+};
 
 function clearTimeouts() {
   for (var timeoutHandle of timeoutHandles) {
@@ -37,31 +34,35 @@ function clearTimeouts() {
 
 var fetchAndCacheToken = function (options, callback) {
   // Get the token
-  request
-    .get(options.url + '/api/v1/xapp_token')
-    .query({ client_id: options.id, client_secret: options.secret })
-    .end(function(err, res) {
-      if (err) {
-        module.exports.emit('error', err);
-        if (callback) callback(err);
-        return
-      }
-
+  fetch(
+    options.url +
+      `/api/v1/xapp_token?client_id=${options.id}&client_secret=${options.secret}`
+  )
+    .then((data) => data.json())
+    .then((res) => {
       // Set and callback w/ token (useful for client middleware).
-      module.exports.token = res.body.xapp_token;
+      module.exports.token = res.xapp_token;
       if (callback) callback(null, module.exports.token);
 
       // Recurse this function to refresh the token it before it expires
-      var expiresAt = new Date(res.body.expires_in).getTime();
-      var timeout = (expiresAt - 1000) - Date.now()
-      var timeoutHandle = setTimeout(function () {
-        var index = timeoutHandles.indexOf(timeoutHandle);
-        timeoutHandles.splice(index, 1);
-        fetchAndCacheToken(options, callback);
-      },
-        ((timeout > MAX_NODE_TIMEOUT) ? MAX_NODE_TIMEOUT :
-         (timeout < MIN_NODE_TIMEOUT) ? MIN_NODE_TIMEOUT : timeout)
-      )
+      var expiresAt = new Date(res.expires_in).getTime();
+      var timeout = expiresAt - 1000 - Date.now();
+      var timeoutHandle = setTimeout(
+        function () {
+          var index = timeoutHandles.indexOf(timeoutHandle);
+          timeoutHandles.splice(index, 1);
+          fetchAndCacheToken(options, callback);
+        },
+        timeout > MAX_NODE_TIMEOUT
+          ? MAX_NODE_TIMEOUT
+          : timeout < MIN_NODE_TIMEOUT
+          ? MIN_NODE_TIMEOUT
+          : timeout
+      );
       timeoutHandles.push(timeoutHandle);
+    })
+    .catch((err) => {
+      module.exports.emit("error", err);
+      if (callback) callback(err);
     });
-}
+};
